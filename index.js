2d0fb0090d02283ce4bfbe6fdc02999a4d7ac6cb
kazawai <bazinmaxime0@gmail.com>
2023-10-04 17:14:52 +0200

    Added auto focus on input

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 
 const command = document.getElementById('title-command');
+const command_input = document.getElementById('command-input');
+command_input.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+  }
+});
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter' && document.activeElement === command_input) {
+    var searchCommand = command_input.innerText;
+    searchCommand.replace(/(<([^>]+)>)/gi, "");
+    window.location.href = `https://www.google.com/search?q=${searchCommand}`;
+  }
+});
+
 command.addEventListener('click', () => {
   console.log('clicked');
-  const command_input = document.getElementById('command-input');
-  command_input.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') {
-      e.preventDefault();
-    }
-  });
-  window.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-      var searchCommand = command_input.innerText;
-      searchCommand.replace(/(<([^>]+)>)/gi, "");
-      window.location.href = `https://www.google.com/search?q=${searchCommand}`;
-    }
-  });
   command_input.focus();
 });
+
+window.onload = () => {
+  command_input.focus();
+}
